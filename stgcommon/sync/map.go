9e0dd7ad63732cfa7767284ba2c7d0162084d671
jerrylou <gunsluo@gmail.com>
2017-08-08 14:44:12 +0800

    sync map
    Signed-off-by: jerrylou <gunsluo@gmail.com>

---
+++ stgcommon/sync/map.go
@@ -0,0 +1,16 @@
+package sync
+
+import concurrent "github.com/fanliao/go-concurrentMap"
+
+// Map 线程安全的map
+type Map struct {
+	// 使用ConcurrentMap，Go1.9版本改用sync/syncmap包实现
+	*concurrent.ConcurrentMap
+}
+
+// NewMap 返回线程安全map
+func NewMap() *Map {
+	return &Map{
+		ConcurrentMap: concurrent.NewConcurrentMap(),
+	}
+}
