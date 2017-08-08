7e23b3177f468a3a4ba858fe4849cbc42379f17a
tantexian <tantexian@qq.com>
2017-08-08 10:49:56 +0800

    增加mappedfile_queue

---
+++ stgstorelog/allocatemaped_fileservice.go
@@ -0,0 +1,8 @@
+// Copyright (c) 2015-2018 All rights reserved.
+// 本软件源代码版权归 my.oschina.net/tantexian 所有,允许复制与学习借鉴.
+// Author: tantexian, <my.oschina.net/tantexian>
+// Since: 17/8/7
+package stgstorelog
+
+type AllocateMapedFileService struct {
+}
