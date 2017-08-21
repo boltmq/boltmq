d467fc530827163b0934ce212bfa758fe605e82f
jerrylou <gunsluo@gmail.com>
2017-08-21 17:23:04 +0800

    remove to stgnet
    Signed-off-by: jerrylou <gunsluo@gmail.com>

--- stgnet/netm/logger.go
+++ stgnet/netm/logger.go
@@ -2,19 +2,29 @@ package netm
 
 import "git.oschina.net/cloudzone/smartgo/stgcommon/logger"
 
-// Fatalf logs a fatal error
-func (bootstrap *Bootstrap) Fatalf(format string, v ...interface{}) {
-	logger.Fatalf(format, v...)
-}
-
 // Debugf logs a debug statement
 func (bootstrap *Bootstrap) Debugf(format string, v ...interface{}) {
 	logger.Debugf(format, v...)
 }
 
+// Tracef logs a trace statement
+func (bootstrap *Bootstrap) Tracef(format string, v ...interface{}) {
+	logger.Tracef(format, v...)
+}
+
 // Noticef logs a notice statement
 func (bootstrap *Bootstrap) Noticef(format string, v ...interface{}) {
 	logger.Infof(format, v...)
+}
+
+// Errorf logs an error
+func (bootstrap *Bootstrap) Errorf(format string, v ...interface{}) {
+	logger.Errorf(format, v...)
+}
+
+// Fatalf logs a fatal error
+func (bootstrap *Bootstrap) Fatalf(format string, v ...interface{}) {
+	logger.Fatalf(format, v...)
 }
 
 // LogFlush logs flush
