de3cd7a44b18721e08c74a1062abc1de7e35280c
jerrylou <gunsluo@gmail.com>
2017-08-21 15:48:43 +0800

    netm handle connect
    Signed-off-by: jerrylou <gunsluo@gmail.com>

--- example/remoting/netm/client/client.go
+++ example/remoting/netm/client/client.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"runtime"
+
 	"git.oschina.net/cloudzone/smartgo/stgremoting/netm"
 )
 
 func main() {
 	b := netm.NewBootstrap()
 	b.Connect("10.122.1.200", 8000)
+	runtime.Goexit()
 }
--- stgremoting/netm/logger.go
+++ stgremoting/netm/logger.go
@@ -4,18 +4,15 @@ import "git.oschina.net/cloudzone/smartgo/stgcommon/logger"
 
 // Fatalf logs a fatal error
 func (bootstrap *Bootstrap) Fatalf(format string, v ...interface{}) {
-	//s.executeLogCall(func(logger Logger, format string, v ...interface{}) {
-	//	logger.Fatalf(format, v...)
-	//}, format, v...)
-	logger.Fatal(format, v...)
+	logger.Fatalf(format, v...)
 }
 
 // Debugf logs a debug statement
 func (bootstrap *Bootstrap) Debugf(format string, v ...interface{}) {
-	logger.Debug(format, v...)
+	logger.Debugf(format, v...)
 }
 
 // Noticef logs a notice statement
 func (bootstrap *Bootstrap) Noticef(format string, v ...interface{}) {
-	logger.Info(format, v...)
+	logger.Infof(format, v...)
 }
