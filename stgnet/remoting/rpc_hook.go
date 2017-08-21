413d6684b1c9c5b7fd390f691054c9a986c83c45
jerrylou <gunsluo@gmail.com>
2017-08-21 19:59:26 +0800

    annotation
    Signed-off-by: jerrylou <gunsluo@gmail.com>

--- stgnet/remoting/rpc_hook.go
+++ stgnet/remoting/rpc_hook.go
@@ -2,6 +2,7 @@ package remoting
 
 import "git.oschina.net/cloudzone/smartgo/stgnet/protocol"
 
+// RPCHook rpc hook, use send msg
 type RPCHook interface {
 	DoBeforeRequest(remoteAddr string, request *protocol.RemotingCommand)
 	DoAfterResponse(remoteAddr string, request *protocol.RemotingCommand, response *protocol.RemotingCommand)
