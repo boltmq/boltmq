155761d9f2c7b9bf32f3cdbd426394f2d9c6654a
jerrylou <gunsluo@gmail.com>
2017-08-22 15:06:59 +0800

    name server choosed
    Signed-off-by: jerrylou <gunsluo@gmail.com>

--- stgnet/remoting/default_remoting_client.go
+++ stgnet/remoting/default_remoting_client.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"net"
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"git.oschina.net/cloudzone/smartgo/stgnet/netm"
@@ -15,13 +16,15 @@ import (
 
 // DefalutRemotingClient default remoting client
 type DefalutRemotingClient struct {
-	bootstrap          *netm.Bootstrap
-	responseTable      map[int32]*ResponseFuture
-	responseTableLock  sync.RWMutex
-	rpcHook            RPCHook
-	namesrvAddrList    []string
-	namesrvAddrChoosed string
-	isClosed           bool
+	bootstrap           *netm.Bootstrap
+	responseTable       map[int32]*ResponseFuture
+	responseTableLock   sync.RWMutex
+	rpcHook             RPCHook
+	namesrvAddrList     []string
+	namesrvAddrListLock sync.RWMutex
+	namesrvAddrChoosed  string
+	namesrvIndex        uint32
+	isClosed            bool
 }
 
 // NewDefalutRemotingClient return new default remoting client
@@ -48,13 +51,37 @@ func (rc *DefalutRemotingClient) Shutdown() {
 
 // GetNameServerAddressList return nameserver addr list
 func (rc *DefalutRemotingClient) GetNameServerAddressList() []string {
+	rc.namesrvAddrListLock.RLock()
+	defer rc.namesrvAddrListLock.RUnlock()
 	return rc.namesrvAddrList
 }
 
-// InvokeSync 同步调用并返回响应
+// UpdateNameServerAddressList update nameserver addrs list
+func (rc *DefalutRemotingClient) UpdateNameServerAddressList(addrs []string) {
+	var (
+		repeat bool
+	)
+
+	rc.namesrvAddrListLock.Lock()
+	for _, addr := range addrs {
+		// 去除重复地址
+		for _, oaddr := range rc.namesrvAddrList {
+			if addr == oaddr {
+				repeat = true
+				break
+			}
+		}
+
+		if repeat == false {
+			rc.namesrvAddrList = append(rc.namesrvAddrList, addr)
+		}
+	}
+	rc.namesrvAddrListLock.Unlock()
+}
+
+// InvokeSync 同步调用并返回响应, addr为空字符串，则在namesrvAddrList中选择地址
 func (rc *DefalutRemotingClient) InvokeSync(addr string, request *protocol.RemotingCommand, timeoutMillis int64) (*protocol.RemotingCommand, error) {
-	// 创建连接，如果连接存在，则不会创建。
-	err := rc.bootstrap.ConnectJoinAddr(addr)
+	err := rc.createConnectByAddr(addr)
 	if err != nil {
 		return nil, err
 	}
@@ -99,8 +126,7 @@ func (rc *DefalutRemotingClient) invokeSync(addr string, request *protocol.Remot
 
 // InvokeAsync 异步调用
 func (rc *DefalutRemotingClient) InvokeAsync(addr string, request *protocol.RemotingCommand, timeoutMillis int64, invokeCallback InvokeCallback) error {
-	// 创建连接，如果连接存在，则不会创建。
-	err := rc.bootstrap.ConnectJoinAddr(addr)
+	err := rc.createConnectByAddr(addr)
 	if err != nil {
 		return err
 	}
@@ -134,8 +160,7 @@ func (rc *DefalutRemotingClient) invokeAsync(addr string, request *protocol.Remo
 
 // InvokeSync 单向发送消息
 func (rc *DefalutRemotingClient) InvokeOneway(addr string, request *protocol.RemotingCommand, timeoutMillis int64) error {
-	// 创建连接，如果连接存在，则不会创建。
-	err := rc.bootstrap.ConnectJoinAddr(addr)
+	err := rc.createConnectByAddr(addr)
 	if err != nil {
 		return err
 	}
@@ -158,6 +183,49 @@ func (rc *DefalutRemotingClient) invokeOneway(addr string, request *protocol.Rem
 	}
 
 	return nil
+}
+
+func (rc *DefalutRemotingClient) createConnectByAddr(addr string) error {
+	if addr == "" {
+		addr = rc.chooseNameseverAddr()
+	}
+
+	// 创建连接，如果连接存在，则不会创建。
+	return rc.bootstrap.ConnectJoinAddr(addr)
+}
+
+func (rc *DefalutRemotingClient) chooseNameseverAddr() string {
+	if rc.namesrvAddrChoosed != "" {
+		return rc.namesrvAddrChoosed
+		// 判断连接是否可用，不可用选取其它name server addr
+		//if rc.bootstrap.HasConnect(rc.namesrvAddrChoosed) {
+		//return rc.namesrvAddrChoosed
+		//}
+	}
+
+	var (
+		caddr string
+		nlen  uint32
+		i     uint32
+	)
+	rc.namesrvAddrListLock.RLock()
+	nlen = uint32(len(rc.namesrvAddrList))
+	for ; i < nlen; i++ {
+		atomic.AddUint32(&rc.namesrvIndex, 1)
+		idx := rc.namesrvIndex % nlen
+
+		newAddr := rc.namesrvAddrList[idx]
+		rc.namesrvAddrChoosed = newAddr
+		caddr = rc.namesrvAddrChoosed
+		break
+		//if rc.bootstrap.HasConnect(newAddr) {
+		//	caddr = rc.namesrvAddrChoosed
+		//	break
+		//}
+	}
+	rc.namesrvAddrListLock.RUnlock()
+
+	return caddr
 }
 
 /*
