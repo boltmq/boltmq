fb21e9ce58e516fc028c59c9248a0f1690c3b555
adaihao <gyl_adaihao@163.com>
2017-08-22 21:50:46 +0800

    ADD broker

---
+++ stgcommon/protocol/header/namesrv/un_register_broker_request_header.go
@@ -0,0 +1,23 @@
+package namesrv
+
+// UnRegisterBrokerRequestHeader 注销broker头信息
+// Author gaoyanlei
+// Since 2017/8/22
+type UnRegisterBrokerRequestHeader struct {
+
+	// broker名字
+	BrokerName string
+
+	// broker地址
+	BrokerAddr string
+
+	// 集群名字
+	ClusterName string
+
+	// brokerId
+	BrokerId int
+}
+
+func(self *UnRegisterBrokerRequestHeader) CheckFields()  {
+
+}
