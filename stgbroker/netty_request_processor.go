050ada5f6fdbe9cb36039e2fb3fa27c16cde0e1e
adaihao <gyl_adaihao@163.com>
2017-08-23 19:32:07 +0800

    ADD broker

--- stgbroker/netty_request_processor.go
+++
@@ -1,8 +0,0 @@
-package stgbroker
-
-import "git.oschina.net/cloudzone/smartgo/stgnet/protocol"
-
-type NettyRequestProcessor interface {
-	ProcessRequest(request protocol.RemotingCommand, // TODO ChannelHandlerContext ctx
-	) protocol.RemotingCommand
-}
