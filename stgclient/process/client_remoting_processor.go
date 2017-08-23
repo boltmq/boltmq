ba71b7f4aed46f0a3ad3584524de1638f7c5d86a
yintongjiang <261026193@qq.com>
2017-08-23 16:59:56 +0800

    update

--- stgclient/process/client_remoting_processor.go
+++ stgclient/process/client_remoting_processor.go
@@ -1,5 +1,11 @@
 package process
 
+import (
+	"net"
+	"git.oschina.net/cloudzone/smartgo/stgnet/protocol"
+	cprotocol"git.oschina.net/cloudzone/smartgo/stgcommon/protocol"
+)
+
 // 客户端处理器
 // Author: yintongqiang
 // Since:  2017/8/8
@@ -14,4 +20,14 @@ func NewClientRemotingProcessor(mqClientFactory *MQClientInstance) *ClientRemoti
 		MQClientFactory:mqClientFactory    }
 }
 
+func (processor*ClientRemotingProcessor)ProcessRequest(addr string, conn net.Conn, request *protocol.RemotingCommand) (*protocol.RemotingCommand, error) {
+	switch request.Code {
+	case cprotocol.NOTIFY_CONSUMER_IDS_CHANGED:
+		processor.notifyConsumerIdsChanged()
+	}
+	return nil, nil
+}
 
+func (processor*ClientRemotingProcessor) notifyConsumerIdsChanged() {
+	processor.MQClientFactory.rebalanceImmediately()
+}
--- stgcommon/protocol/heartbeat/heartbeat_data.go
+++ stgcommon/protocol/heartbeat/heartbeat_data.go
@@ -1,12 +1,12 @@
 package heartbeat
 
-import 	set "github.com/deckarep/golang-set"
+import set "github.com/deckarep/golang-set"
 // HeartbeatData 客户端与broker心跳结构体
 // Author: yintongqiang
 // Since:  2017/8/8
 
 type HeartbeatData struct {
-  ClientID string
-  ProducerDataSet set.Set
-  ConsumerDataSet set.Set
+	ClientID        string  `json:"clientID"`
+	ProducerDataSet set.Set `json:"producerDataSet"`
+	ConsumerDataSet set.Set `json:"consumerDataSet"`
 }
