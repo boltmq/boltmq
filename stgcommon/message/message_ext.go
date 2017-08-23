3c7cc5b02635dbbfb7b909c1238256e05c5a98ff
jerrylou <gunsluo@gmail.com>
2017-08-23 17:49:46 +0800

    decode message ext
    Signed-off-by: jerrylou <gunsluo@gmail.com>

--- stgcommon/message/message_ext.go
+++ stgcommon/message/message_ext.go
@@ -1,7 +1,4 @@
 package message
-// MessageExt: 消息扩展
-// Author: yintongqiang
-// Since:  2017/8/10
 
 type MessageExt struct {
 	//// 消息主题
@@ -14,13 +11,13 @@ type MessageExt struct {
 	//Body                      []byte
 	Message
 	// 队列ID <PUT>
-	QueueId                   int
+	QueueId                   int32
 	// 存储记录大小
-	StoreSize                 int
+	StoreSize                 int32
 	// 队列偏移量
 	QueueOffset               int64
 	// 消息标志位 <PUT>
-	SysFlag                   int
+	SysFlag                   int32
 	// 消息在客户端创建时间戳 <PUT>
 	BornTimestamp             int64
 	// 消息来自哪里 <PUT>
@@ -34,8 +31,8 @@ type MessageExt struct {
 	// 消息对应的Commit Log Offset
 	CommitLogOffset           int64
 	// 消息体CRC
-	BodyCRC                   int
+	BodyCRC                   int32
 	// 当前消息被某个订阅组重新消费了几次（订阅组之间独立计数）
-	ReconsumeTimes            int
+	ReconsumeTimes            int32
 	PreparedTransactionOffset int64
 }
