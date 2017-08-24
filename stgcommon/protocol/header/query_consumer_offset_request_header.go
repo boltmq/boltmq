c25dd12901927365fdef0522c4e8c7d569629b6c
yintongjiang <261026193@qq.com>
2017-08-24 14:05:44 +0800

    update

---
+++ stgcommon/protocol/header/query_consumer_offset_request_header.go
@@ -0,0 +1,14 @@
+package header
+// QueryConsumerOffsetRequestHeader: 查询消费offset
+// Author: yintongqiang
+// Since:  2017/8/24
+type QueryConsumerOffsetRequestHeader struct {
+	ConsumerGroup string `json:"consumerGroup"`
+	Topic         string `json:"topic"`
+	QueueId       int32 `json:"queueId"`
+}
+
+
+func (header*QueryConsumerOffsetRequestHeader)CheckFields(){
+
+}
