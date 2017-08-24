a6c374eb786c833f48ea3131ff170d3d452d6515
adaihao <gyl_adaihao@163.com>
2017-08-24 18:29:32 +0800

    ADD broker

--- stgbroker/admin_broker_processor.go
+++ stgbroker/admin_broker_processor.go
@@ -1,13 +1,14 @@
 package stgbroker
 
 import (
-	"git.oschina.net/cloudzone/smartgo/stgnet/protocol"
-	protocol2 "git.oschina.net/cloudzone/smartgo/stgcommon/protocol"
-	"net"
-	"git.oschina.net/cloudzone/smartgo/stgcommon/protocol/header"
-	code "git.oschina.net/cloudzone/smartgo/stgcommon/protocol"
-	"strings"
+	"git.oschina.net/cloudzone/smartgo/stgcommon"
 	"git.oschina.net/cloudzone/smartgo/stgcommon/logger"
+	code "git.oschina.net/cloudzone/smartgo/stgcommon/protocol"
+	protocol2 "git.oschina.net/cloudzone/smartgo/stgcommon/protocol"
+	"git.oschina.net/cloudzone/smartgo/stgcommon/protocol/header"
+	"git.oschina.net/cloudzone/smartgo/stgnet/protocol"
+	"net"
+	"strings"
 )
 
 // AdminBrokerProcessor 管理类请求处理
@@ -34,18 +35,29 @@ func (abp *AdminBrokerProcessor) ProcessRequest(addr string, conn net.Conn, requ
 	return nil, nil
 }
 
-func (cmp *AdminBrokerProcessor) updateAndCreateTopic(addr string, conn net.Conn, request *protocol.RemotingCommand) (*protocol.RemotingCommand, error) {
+func (abp *AdminBrokerProcessor) updateAndCreateTopic(addr string, conn net.Conn, request *protocol.RemotingCommand) (*protocol.RemotingCommand, error) {
 	response := &protocol.RemotingCommand{}
-	requestHeader:=&header.CreateTopicRequestHeader{}
-	if strings.EqualFold(requestHeader.Topic,cmp.BrokerController.BrokerConfig.BrokerClusterName) {
+	requestHeader := &header.CreateTopicRequestHeader{}
+	if strings.EqualFold(requestHeader.Topic, abp.BrokerController.BrokerConfig.BrokerClusterName) {
 		errorMsg :=
 			"the topic[" + requestHeader.Topic + "] is conflict with system reserved words."
 		logger.Warn(errorMsg)
-		response.Code= code.SYSTEM_ERROR
-		response.Remark= errorMsg
-		return response,nil
+		response.Code = code.SYSTEM_ERROR
+		response.Remark = errorMsg
+		return response, nil
 	}
 
+	topicConfig := &stgcommon.TopicConfig{
+		ReadQueueNums:   requestHeader.ReadQueueNums,
+		WriteQueueNums:  requestHeader.WriteQueueNums,
+		TopicFilterType: requestHeader.TopicFilterType,
+		Perm:            requestHeader.Perm,
+	}
+	if requestHeader.TopicSysFlag != 0 {
+		topicConfig.TopicSysFlag = requestHeader.TopicSysFlag
+	}
+	abp.BrokerController.TopicConfigManager.UpdateTopicConfig(topicConfig)
+	abp.BrokerController.RegisterBrokerAll(false, true)
 	response.Code = code.SUCCESS
 	response.Remark = ""
 	return response, nil
--- stgcommon/protocol/header/create_topic_request_header.go
+++ stgcommon/protocol/header/create_topic_request_header.go
@@ -1,4 +1,7 @@
 package header
+
+import "git.oschina.net/cloudzone/smartgo/stgcommon"
+
 // CreateTopicRequestHeader: 创建topic头信息
 // Author: yintongqiang
 // Since:  2017/8/17
@@ -9,7 +12,7 @@ type CreateTopicRequestHeader struct {
 	ReadQueueNums   int32
 	WriteQueueNums  int32
 	Perm            int
-	TopicFilterType string
+	TopicFilterType stgcommon.TopicFilterType
 	TopicSysFlag    int
 	Order           bool
 }
