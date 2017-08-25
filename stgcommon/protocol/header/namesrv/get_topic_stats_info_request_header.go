abdf7d99ae9bb3ae12ef42e69be092de05e11fee
jerrylou <gunsluo@gmail.com>
2017-08-25 10:42:55 +0800

    fix client server communication
    Signed-off-by: jerrylou <gunsluo@gmail.com>

---
+++ stgcommon/protocol/header/namesrv/get_topic_stats_info_request_header.go
@@ -0,0 +1,12 @@
+package namesrv
+
+// GetTopicStatsInfoRequestHeader 获取topic 统计信息头
+// Author: jerrylou, <gunsluo@gmail.com>
+// Since: 2017-08-25
+type GetTopicStatsInfoRequestHeader struct {
+	Topic string
+}
+
+func (header *GetTopicStatsInfoRequestHeader) CheckFields() error {
+	return nil
+}
