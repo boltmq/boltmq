dd26b8c32e8b9eede989590ea90da30b6b74c5ed
jerrylou <gunsluo@gmail.com>
2017-08-29 16:10:25 +0800

    modify length field frame packet test
    Signed-off-by: jerrylou <gunsluo@gmail.com>

--- stgnet/remoting/length_field_frame_packet_test.go
+++ stgnet/remoting/length_field_frame_packet_test.go
@@ -10,6 +10,29 @@ import (
 func TestUnpackFull(t *testing.T) {
 	var (
 		addr                   = "192.168.0.1:10000"
+		lengthFieldFramePacket = NewLengthFieldFramePacket(8388608, 0, 4, 0)
+	)
+
+	buffer := prepareFullBuffer()
+
+	bufs, e := lengthFieldFramePacket.UnPack(addr, buffer)
+	if e != nil {
+		t.Errorf("Test failed: %s", e)
+	}
+
+	if len(bufs) != 1 {
+		t.Errorf("Test failed: return buf size[%d] incorrect, expect[%d]", len(bufs), 1)
+	}
+
+	buf := bufs[0]
+	if !reflect.DeepEqual(buf.Bytes(), buffer) {
+		t.Errorf("Test failed: return buf%v incorrect, expect%v", buf.Bytes(), buffer)
+	}
+}
+
+func TestUnpackOffsetFull(t *testing.T) {
+	var (
+		addr                   = "192.168.0.1:10000"
 		lengthFieldFramePacket = NewLengthFieldFramePacket(8388608, 0, 4, 4)
 	)
 
@@ -26,7 +49,7 @@ func TestUnpackFull(t *testing.T) {
 
 	buf := bufs[0]
 	if !reflect.DeepEqual(buf.Bytes(), buffer[4:]) {
-		t.Errorf("Test failed: return buf%v incorrect, expect%v", buf.Bytes(), buffer)
+		t.Errorf("Test failed: return buf%v incorrect, expect%v", buf.Bytes(), buffer[4:])
 	}
 }
 
@@ -42,6 +65,40 @@ func prepareFullBuffer() []byte {
 }
 
 func TestUnpackPartOf(t *testing.T) {
+	var (
+		addr                   = "192.168.0.1:10000"
+		lengthFieldFramePacket = NewLengthFieldFramePacket(8388608, 0, 4, 0)
+	)
+
+	buffer := preparePartOfOneBuffer()
+	bufs, e := lengthFieldFramePacket.UnPack(addr, buffer)
+	if e != nil {
+		t.Errorf("Test failed: %s", e)
+	}
+
+	if len(bufs) != 0 {
+		t.Errorf("Test failed: return buf size[%d] incorrect, expect[%d]", len(bufs), 0)
+	}
+	bufferHeader := buffer
+
+	buffer = preparePartOfTwoBuffer()
+	bufs, e = lengthFieldFramePacket.UnPack(addr, buffer)
+	if e != nil {
+		t.Errorf("Test failed: %s", e)
+	}
+
+	if len(bufs) != 1 {
+		t.Errorf("Test failed: return buf size[%d] incorrect, expect[%d]", len(bufs), 1)
+	}
+
+	buf := bufs[0]
+	allBytes := append(bufferHeader, buffer...)
+	if !reflect.DeepEqual(buf.Bytes(), allBytes) {
+		t.Errorf("Test failed: return buf%v incorrect, expect%v", buf.Bytes(), allBytes)
+	}
+}
+
+func TestUnpackOffsetPartOf(t *testing.T) {
 	var (
 		addr                   = "192.168.0.1:10000"
 		lengthFieldFramePacket = NewLengthFieldFramePacket(8388608, 0, 4, 4)
@@ -97,6 +154,41 @@ func preparePartOfTwoBuffer() []byte {
 func TestUnpackPartOf2(t *testing.T) {
 	var (
 		addr                   = "192.168.0.1:10000"
+		lengthFieldFramePacket = NewLengthFieldFramePacket(8388608, 0, 4, 0)
+	)
+
+	buffer := preparePartOfOneBuffer2()
+	bufs, e := lengthFieldFramePacket.UnPack(addr, buffer)
+	if e != nil {
+		t.Errorf("Test failed: %s", e)
+	}
+
+	if len(bufs) != 0 {
+		t.Errorf("Test failed: return buf size[%d] incorrect, expect[%d]", len(bufs), 0)
+	}
+	bufferHeader := buffer
+
+	buffer = preparePartOfTwoBuffer2()
+	bufs, e = lengthFieldFramePacket.UnPack(addr, buffer)
+	if e != nil {
+		t.Errorf("Test failed: %s", e)
+	}
+
+	if len(bufs) != 1 {
+		t.Errorf("Test failed: return buf size[%d] incorrect, expect[%d]", len(bufs), 1)
+	}
+
+	buf := bufs[0]
+	allBytes := append(bufferHeader, buffer...)
+	allBytes = allBytes[:buf.Len()]
+	if !reflect.DeepEqual(buf.Bytes(), allBytes) {
+		t.Errorf("Test failed: return buf%v incorrect, expect%v", buf.Bytes(), allBytes)
+	}
+}
+
+func TestUnpackOffsetPartOf2(t *testing.T) {
+	var (
+		addr                   = "192.168.0.1:10000"
 		lengthFieldFramePacket = NewLengthFieldFramePacket(8388608, 0, 4, 4)
 	)
 
@@ -150,6 +242,33 @@ func preparePartOfTwoBuffer2() []byte {
 	buf.Write([]byte("abc"))
 
 	return buf.Bytes()
+}
+
+func TestDiscardUnpackOffsetPartOf(t *testing.T) {
+	var (
+		addr                   = "192.168.0.1:10000"
+		lengthFieldFramePacket = NewLengthFieldFramePacket(8388608, 0, 4, 0)
+	)
+
+	buffer := preparePartOfOneDiscardBuffer()
+	bufs, e := lengthFieldFramePacket.UnPack(addr, buffer)
+	if e != nil {
+		t.Logf("UnPack failed: %s", e)
+	}
+
+	if len(bufs) != 0 {
+		t.Errorf("Test failed: return buf size[%d] incorrect, expect[%d]", len(bufs), 0)
+	}
+
+	buffer = preparePartOfTwoDiscardBuffer()
+	bufs, e = lengthFieldFramePacket.UnPack(addr, buffer)
+	if e != nil {
+		t.Errorf("Test failed: %s", e)
+	}
+
+	if len(bufs) != 0 {
+		t.Errorf("Test failed: return buf size[%d] incorrect, expect[%d]", len(bufs), 0)
+	}
 }
 
 func TestDiscardUnpackPartOf(t *testing.T) {
