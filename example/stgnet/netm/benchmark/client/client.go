3e295c963e39bae1c4b0412be7531d2cd4c61340
jerrylou <gunsluo@gmail.com>
2017-08-30 11:00:56 +0800

    client statis
    Signed-off-by: jerrylou <gunsluo@gmail.com>

--- example/stgnet/netm/benchmark/client/client.go
+++ example/stgnet/netm/benchmark/client/client.go
@@ -15,16 +15,28 @@ func main() {
 	mcn := flag.Int("c", 50000, "max connect num")
 	flag.Parse()
 
+	var (
+		maxConnNum int
+		connTotal  int
+		sendTotal  int
+		receTotal  int
+		cStartTime time.Time
+		cEndTime   time.Time
+		cd         time.Duration
+		sStartTime time.Time
+		sEndTime   time.Time
+		sd         time.Duration
+	)
 	b := netm.NewBootstrap()
 	b.RegisterHandler(func(buffer []byte, addr string, conn net.Conn) {
-		fmt.Println("rece:", string(buffer))
+		receTotal++
+		fmt.Println("receive:", receTotal, string(buffer))
 	})
 
 	// 创建连接
 	var conns []net.Conn
-	var maxConnNum int
 	maxConnNum = *mcn
-	start := time.Now()
+	cStartTime = time.Now()
 	for i := 0; i < maxConnNum; i++ {
 		conn, err := b.NewRandomConnect(*host, *port)
 		if err != nil {
@@ -32,26 +44,35 @@ func main() {
 			break
 		}
 		conns = append(conns, conn)
+		connTotal++
 		//time.Sleep(10 * time.Microsecond)
 	}
-	d := time.Now().Sub(start)
+	cEndTime = time.Now()
+	cd = cEndTime.Sub(cStartTime)
 
-	var total int
 	msg := "hello netm"
-	fmt.Printf("msg content: %s\n", msg)
-	start = time.Now()
+	//fmt.Printf("msg content: %s\n", msg)
+	sStartTime = time.Now()
 	for _, conn := range conns {
 		_, err := conn.Write([]byte(msg))
 		if err != nil {
 			fmt.Printf("send msg faild: %s\n", err)
 			continue
 		}
-		total++
+		sendTotal++
 	}
+	sEndTime = time.Now()
+	sd = sEndTime.Sub(sStartTime)
 
-	fmt.Printf("connect num: %d spend %v\n", len(conns), d)
-	d = time.Now().Sub(start)
-	fmt.Printf("send msg: %d spend %v\n", total, d)
+	go func() {
+		timer := time.NewTimer(1 * time.Second)
+		for {
+			<-timer.C
+			timer.Reset(10 * time.Second)
+			fmt.Printf("create connect success [%d], failed[%d], spend time %v| send msg success [%d], failed[%d], spend time %v| receive msg success [%d], failed[%d]\n",
+				connTotal, maxConnNum-connTotal, cd, sendTotal, connTotal-sendTotal, sd, receTotal, sendTotal-receTotal)
+		}
+	}()
 
 	select {}
 }
