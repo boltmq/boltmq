2b8a8e652c02e56c0fdad3ec1e47bdbb38eb12f9
idistyl <idistyl@gmail.com>
2017-09-07 00:50:07 +0800

    feture 增加部分namesrv_colltroller主要处理业务

--- stgregistry/controller/namesrv_controller.go
+++ stgregistry/controller/namesrv_controller.go
@@ -4,29 +4,35 @@ import (
 	"git.oschina.net/cloudzone/smartgo/stgcommon/namesrv"
 	//"git.oschina.net/cloudzone/smartgo/stgregistry/kvconfig"
 	//"git.oschina.net/cloudzone/smartgo/stgregistry/routeinfo"
+	"git.oschina.net/cloudzone/smartgo/stgnet/remoting"
+	_ "git.oschina.net/cloudzone/smartgo/stgregistry/processor"
 )
 
 // NamesrvController 注意循环引用
 // Author: tianyuliang, <tianyuliang@gome.com.cn>
 // Since: 2017/9/6
 type NamesrvController struct {
-	NamesrvConfig *namesrv.NamesrvConfig
+	NamesrvConfig  *namesrv.NamesrvConfig
+	RemotingServer *remoting.RemotingServer
 	//RouteInfoManager routeinfo.RouteInfoManager
 	//KvConfigManager  kvconfig.KVConfigSerializeWrapper
 }
 
 func (self *NamesrvController) shutdown() {
+	//RemotingServer := remoting.NewDefalutRemotingClient()
 
+	//defaultRequestProcessor := processor.NewDefaultRequestProcessor(self)
+	//RemotingServer.RegisterDefaultProcessor(nil)
 }
 
-func start() error {
+func (self *NamesrvController) start() error {
 	return nil
 }
 
-func registerProcessor() {
+func (self *NamesrvController) registerProcessor() {
 
 }
 
-func initialize() bool {
+func (self *NamesrvController) initialize() bool {
 	return false
 }
