bc297006b16e785f60f54d4a135e8de8ef19cf97
adaihao <gyl_adaihao@163.com>
2017-09-08 15:36:20 +0800

    UPDATE BROKER

--- stgbroker/abstract_send_message_processor.go
+++ stgbroker/abstract_send_message_processor.go
@@ -89,7 +89,7 @@ func (asmp *AbstractSendMessageProcessor) msgCheck(conn net.Conn, requestHeader 
 			}
 		}
 
-		topicConfig, _ := asmp.BrokerController.TopicConfigManager.createTopicInSendMessageMethod(requestHeader.Topic, requestHeader.DefaultTopic,
+		topicConfig, _ = asmp.BrokerController.TopicConfigManager.createTopicInSendMessageMethod(requestHeader.Topic, requestHeader.DefaultTopic,
 			conn.LocalAddr().String(), requestHeader.DefaultTopicQueueNums, topicSysFlag)
 		if topicConfig == nil {
 			if strings.Contains(requestHeader.Topic, stgcommon.RETRY_GROUP_TOPIC_PREFIX) {
