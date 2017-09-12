3a3865689b95805b5a5491d3fa25701743bcb238
zhoufei <he236555699@163.com>
2017-09-12 10:38:04 +0800

    store:新增put、get消息

--- stgcommon/protocol/heartbeat/subscription_data.go
+++ stgcommon/protocol/heartbeat/subscription_data.go
@@ -1,16 +1,17 @@
 package heartbeat
 
 import set "github.com/deckarep/golang-set"
+
 // SubscriptionData: 订阅信息结构体
 // Author: yintongqiang
 // Since:  2017/8/9
 
 type SubscriptionData struct {
-	SUB_ALL         string     `json:"-"`
-	ClassFilterMode bool       `json:"classFilterMode"`
-	Topic           string     `json:"topic"`
-	SubString       string     `json:"subString"`
-	TagsSet         set.Set    `json:"tagsSet"`
-	CodeSet         set.Set    `json:"codeSet"`
-	SubVersion      int        `json:"subVersion"`
+	SUB_ALL         string  `json:"-"`
+	ClassFilterMode bool    `json:"classFilterMode"`
+	Topic           string  `json:"topic"`
+	SubString       string  `json:"subString"`
+	TagsSet         set.Set `json:"tagsSet"`
+	CodeSet         set.Set `json:"codeSet"`
+	SubVersion      int     `json:"subVersion"`
 }
