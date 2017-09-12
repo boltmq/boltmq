19848f481f876d2ee526a6a76ebc809a25d84337
zhoufei <he236555699@163.com>
2017-09-12 10:47:27 +0800

    store:修复编译错误

---
+++ stgstorelog/allocate_maped_file_service.go
@@ -0,0 +1,17 @@
+// Copyright (c) 2015-2018 All rights reserved.
+// 本软件源代码版权归 my.oschina.net/tantexian 所有,允许复制与学习借鉴.
+// Author: tantexian, <tantexian@qq.com>
+// Since: 2017/8/7
+package stgstorelog
+
+type AllocateMapedFileService struct {
+}
+
+func (self *AllocateMapedFileService) putRequestAndReturnMapedFile(nextFilePath string, nextNextFilePath string, filesize int64) (*MapedFile, error) {
+	// TODO: putRequestAndReturnMapedFile待完成
+	return &MapedFile{}, nil
+}
+
+func (self *AllocateMapedFileService) run() {
+	// TODO
+}
