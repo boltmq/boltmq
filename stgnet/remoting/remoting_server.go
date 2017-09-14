afaae9dc5680276f397e73021de3a63a5949bc90
jerrylou <gunsluo@gmail.com>
2017-09-14 17:38:22 +0800

    remoting conntext
    Signed-off-by: jerrylou <gunsluo@gmail.com>

--- stgnet/remoting/remoting_server.go
+++ stgnet/remoting/remoting_server.go
@@ -1,16 +1,15 @@
 package remoting
 
 import (
-	"net"
-
+	"git.oschina.net/cloudzone/smartgo/stgnet/netm"
 	"git.oschina.net/cloudzone/smartgo/stgnet/protocol"
 )
 
 // RemotingServer remoting server define
 type RemotingServer interface {
-	InvokeSync(conn net.Conn, request *protocol.RemotingCommand, timeoutMillis int64) (*protocol.RemotingCommand, error)
-	InvokeAsync(conn net.Conn, request *protocol.RemotingCommand, timeoutMillis int64, invokeCallback InvokeCallback) error
-	InvokeOneway(conn net.Conn, request *protocol.RemotingCommand, timeoutMillis int64) error
+	InvokeSync(ctx netm.Context, request *protocol.RemotingCommand, timeoutMillis int64) (*protocol.RemotingCommand, error)
+	InvokeAsync(ctx netm.Context, request *protocol.RemotingCommand, timeoutMillis int64, invokeCallback InvokeCallback) error
+	InvokeOneway(ctx netm.Context, request *protocol.RemotingCommand, timeoutMillis int64) error
 	RegisterProcessor(requestCode int32, processor RequestProcessor)
 	RegisterDefaultProcessor(processor RequestProcessor)
 	RegisterRPCHook(rpcHook RPCHook)
