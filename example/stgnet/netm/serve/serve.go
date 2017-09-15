800de732612854dab0274bdc828c9de33750dafa
jerrylou <gunsluo@gmail.com>
2017-09-15 13:51:26 +0800

    conntext listener demo
    Signed-off-by: jerrylou <gunsluo@gmail.com>

--- example/stgnet/netm/serve/serve.go
+++ example/stgnet/netm/serve/serve.go
@@ -6,8 +6,27 @@ import (
 	"git.oschina.net/cloudzone/smartgo/stgnet/netm"
 )
 
+type DemoContextListener struct {
+}
+
+func (listener *DemoContextListener) OnContextConnect(ctx netm.Context) {
+	log.Printf("one connection create: addr[%s] localAddr[%s] remoteAddr[%s]\n", ctx.Addr(), ctx.LocalAddr(), ctx.RemoteAddr())
+}
+
+func (listener *DemoContextListener) OnContextClose(ctx netm.Context) {
+	log.Printf("one connection close: addr[%s] localAddr[%s] remoteAddr[%s]\n", ctx.Addr(), ctx.LocalAddr(), ctx.RemoteAddr())
+}
+
+func (listener *DemoContextListener) OnContextError(ctx netm.Context) {
+	log.Printf("one connection error: addr[%s] localAddr[%s] remoteAddr[%s]\n", ctx.Addr(), ctx.LocalAddr(), ctx.RemoteAddr())
+}
+
+func (listener *DemoContextListener) OnContextIdle(ctx netm.Context) {
+	log.Printf("one connection idle: addr[%s] localAddr[%s] remoteAddr[%s]\n", ctx.Addr(), ctx.LocalAddr(), ctx.RemoteAddr())
+}
+
 func main() {
-	b := netm.NewBootstrap()
+	b := netm.NewBootstrap().RegisterContextListener(&DemoContextListener{})
 	b.SetKeepAlive(false).Bind("0.0.0.0", 8000).
 		RegisterHandler(func(buffer []byte, ctx netm.Context) {
 			log.Printf("serve receive msg form %s, local[%s]. msg: %s", ctx.RemoteAddr().String(), ctx.LocalAddr().String(), string(buffer))
