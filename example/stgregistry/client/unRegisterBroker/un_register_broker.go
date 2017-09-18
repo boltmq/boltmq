a822d5d262b01e517bb8c181ea1ebd3eec127f7c
idistyl <idistyl@gmail.com>
2017-09-18 22:55:44 +0800

    feature 初步完成"UNREGISTER_BROKER"接口example示例

--- example/stgregistry/client/unRegisterBroker/un_register_broker.go
+++ example/stgregistry/client/unRegisterBroker/un_register_broker.go
@@ -3,13 +3,11 @@ package main
 import (
 	"fmt"
 	"git.oschina.net/cloudzone/smartgo/example/stgregistry/client"
-	util "git.oschina.net/cloudzone/smartgo/stgcommon/namesrv"
 	code "git.oschina.net/cloudzone/smartgo/stgcommon/protocol"
 	"git.oschina.net/cloudzone/smartgo/stgcommon/protocol/header/namesrv"
 	"git.oschina.net/cloudzone/smartgo/stgnet/protocol"
 	"git.oschina.net/cloudzone/smartgo/stgnet/remoting"
 	"git.oschina.net/cloudzone/smartgo/stgregistry/logger"
-	"strings"
 )
 
 var (
@@ -23,11 +21,13 @@ func initClient() {
 
 func main() {
 	var (
-		request  *protocol.RemotingCommand
-		response *protocol.RemotingCommand
-		err      error
-		key      = "jcpt-example-200"
-		value    = strings.Join([]string{"broker-a:8"}, ";") // 多个value值通过分号;连接
+		request     *protocol.RemotingCommand
+		response    *protocol.RemotingCommand
+		err         error
+		brokerName  = "broker-b"
+		brokerAddr  = "10.122.2.28:10911"
+		clusterName = "DefaultCluster"
+		brokerId    = 0
 	)
 
 	// 初始化
@@ -38,12 +38,13 @@ func main() {
 	fmt.Println("remoting client start success")
 
 	// 请求的custom header
-	requestHeader := &namesrv.PutKVConfigRequestHeader{
-		Namespace: util.NAMESPACE_ORDER_TOPIC_CONFIG,
-		Key:       key,
-		Value:     value,
+	requestHeader := &namesrv.UnRegisterBrokerRequestHeader{
+		BrokerAddr:  brokerAddr,
+		BrokerName:  brokerName,
+		ClusterName: clusterName,
+		BrokerId:    brokerId,
 	}
-	request = protocol.CreateRequestCommand(code.PUT_KV_CONFIG, requestHeader)
+	request = protocol.CreateRequestCommand(code.UNREGISTER_BROKER, requestHeader)
 
 	namesrvAddrs := cmd.GetNameServerAddressList()
 	if namesrvAddrs != nil && len(namesrvAddrs) > 0 {
@@ -51,18 +52,18 @@ func main() {
 			// 同步发送请求
 			response, err = cmd.InvokeSync(namesrvAddr, request, client.DEFAULT_TIMEOUT)
 			if err != nil {
-				logger.Error("sync response PUT_KV_CONFIG failed. err: %s", err.Error())
+				logger.Error("sync response UNREGISTER_BROKER failed. err: %s", err.Error())
 				return
 			}
 			if response == nil {
-				logger.Error("sync response PUT_KV_CONFIG failed. err: response is nil")
+				logger.Error("sync response UNREGISTER_BROKER failed. err: response is nil")
 				return
 			}
 			if response.Code == code.SUCCESS {
-				logger.Info("sync response PUT_KV_CONFIG success.")
+				logger.Info("sync response UNREGISTER_BROKER success.")
 				return
 			}
-			format := "sync handle PUT_KV_CONFIG failed. code=%d, remark=%s"
+			format := "sync handle UNREGISTER_BROKER failed. code=%d, remark=%s"
 			logger.Info(format, response.Code, response.Remark)
 		}
 	}
--- stgcommon/protocol/header/namesrv/un_register_broker_request_header.go
+++ stgcommon/protocol/header/namesrv/un_register_broker_request_header.go
@@ -1,5 +1,10 @@
 package namesrv
 
+import (
+	"fmt"
+	"strings"
+)
+
 // UnRegisterBrokerRequestHeader 注销broker-请求头信息
 // Author gaoyanlei
 // Since 2017/8/22
@@ -11,5 +16,17 @@ type UnRegisterBrokerRequestHeader struct {
 }
 
 func (self *UnRegisterBrokerRequestHeader) CheckFields() error {
+	if strings.TrimSpace(self.BrokerName) == "" {
+		return fmt.Errorf("UnRegisterBrokerRequestHeader.BrokerName is empty")
+	}
+	if strings.TrimSpace(self.BrokerAddr) == "" {
+		return fmt.Errorf("UnRegisterBrokerRequestHeader.BrokerAddr is empty")
+	}
+	if strings.TrimSpace(self.ClusterName) == "" {
+		return fmt.Errorf("UnRegisterBrokerRequestHeader.ClusterName is empty")
+	}
+	if self.BrokerId < 0 {
+		return fmt.Errorf("UnRegisterBrokerRequestHeader.BrokerId is invalid")
+	}
 	return nil
 }
