100239c66995b0f84c00832bcd5c3135e8c0fb78
adaihao <gyl_adaihao@163.com>
2017-09-19 11:48:30 +0800

    UPDATE BROKER

---
+++ stgbroker/broker_outer_api_test.go
@@ -0,0 +1,15 @@
+package stgbroker
+
+import (
+	"testing"
+	"git.oschina.net/cloudzone/smartgo/stgbroker/out"
+)
+
+func TestRegisterBroker(t *testing.T) {
+	brokerController := CreateBrokerController()
+	topicManager:=NewTopicConfigManager(brokerController)
+	topicManager.Load()
+	brokerOuterAPI := out.NewBrokerOuterAPI()
+	brokerOuterAPI.RegisterBroker("10.122.1.210:9876","out","10.122.2.28:10911",
+		"broker-1","10.122.1.20:10912",1,topicManager.TopicConfigSerializeWrapper,false,nil)
+}
