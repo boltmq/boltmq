ac3f378de7dc996f339680ca241ce39f564fc692
jerrylou <gunsluo@gmail.com>
2017-09-19 16:59:51 +0800

    support cross platform
    Signed-off-by: jerrylou <gunsluo@gmail.com>

--- stgstorelog/mmap/mmap_unix.go
+++ stgstorelog/mmap/mmap_unix.go
@@ -1,3 +1,8 @@
+// Copyright 2011 Evan Shaw. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build darwin dragonfly freebsd linux openbsd solaris netbsd
 package mmap
 
 import (
