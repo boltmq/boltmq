685216a519492a91cc29b3130c894261d95da1b3
adaihao <gyl_adaihao@163.com>
2017-09-19 16:04:19 +0800

    UPDATE BROKER

--- stgbroker/broker_outer_api_test.go
+++ stgbroker/broker_outer_api_test.go
@@ -10,6 +10,16 @@ func TestRegisterBroker(t *testing.T) {
 	topicManager:=NewTopicConfigManager(brokerController)
 	topicManager.Load()
 	brokerOuterAPI := out.NewBrokerOuterAPI()
-	brokerOuterAPI.RegisterBroker("10.122.1.210:9876","out","10.122.2.28:10911",
+	brokerOuterAPI.RegisterBroker("0.0.0.0:9876","out","10.122.2.28:10911",
 		"broker-1","10.122.1.20:10912",1,topicManager.TopicConfigSerializeWrapper,false,nil)
+}
+
+func TestNameRegisterBroker(t *testing.T) {
+	//remotingClient:=remoting.NewDefalutRemotingClient()
+	//
+	//topicManager:=NewTopicConfigManager(brokerController)
+	//topicManager.Load()
+	//brokerOuterAPI := out.NewBrokerOuterAPI()
+	//brokerOuterAPI.RegisterBroker("10.122.1.210:9876","out","10.122.2.28:10911",
+	//	"broker-1","10.122.1.20:10912",1,topicManager.TopicConfigSerializeWrapper,false,nil)
 }
