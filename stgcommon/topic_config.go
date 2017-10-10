34eec48bfcbfb5d67b93ba45679f8cbe9da742d6
idistyl <idistyl@gmail.com>
2017-10-10 16:23:01 +0800

    update 优化topic_config.toString()的perm字段的显示效果

--- stgcommon/topic_config.go
+++ stgcommon/topic_config.go
@@ -2,12 +2,16 @@ package stgcommon
 
 import (
 	"fmt"
+	"git.oschina.net/cloudzone/smartgo/stgcommon/constant"
 )
 
 const (
 	DefaultReadQueueNums  = 16
 	DefaultWriteQueueNums = 16
 	separator             = " "
+	perm                  = constant.PERM_READ | constant.PERM_WRITE
+	topicFilterType       = TopicFilterType(SINGLE_TAG)
+	topicSysFlag          = 0
 )
 
 type TopicConfig struct {
@@ -23,23 +27,45 @@ type TopicConfig struct {
 
 func NewTopicConfig(topicName string) *TopicConfig {
 	topicConfig := &TopicConfig{
-		TopicName:      topicName,
-		WriteQueueNums: DefaultWriteQueueNums,
-		ReadQueueNums:  DefaultReadQueueNums,
-		SEPARATOR:      separator,
+		TopicName:       topicName,
+		WriteQueueNums:  DefaultWriteQueueNums,
+		ReadQueueNums:   DefaultReadQueueNums,
+		SEPARATOR:       separator,
+		Perm:            perm,
+		TopicFilterType: topicFilterType,
+		TopicSysFlag:    topicSysFlag,
 	}
 	return topicConfig
 }
 
-func NewDefaultTopicConfig(topicName string, readQueueNums, writeQueueNums int32, perm int, topicFilterType TopicFilterType) *TopicConfig {
+func NewDefaultTopicConfig(topicName string, readQueueNums, writeQueueNums int32, perm int, filterType TopicFilterType) *TopicConfig {
 	topicConfig := &TopicConfig{
 		TopicName:       topicName,
 		WriteQueueNums:  writeQueueNums,
 		ReadQueueNums:   readQueueNums,
 		SEPARATOR:       separator,
 		Perm:            perm,
-		TopicFilterType: topicFilterType,
+		TopicFilterType: filterType,
+		TopicSysFlag:    topicSysFlag,
 	}
+	return topicConfig
+}
+
+func NewCustomTopicConfig(topicName string, readQueueNums, writeQueueNums int32, topicSysFlag int, filterType ...TopicFilterType) *TopicConfig {
+	topicConfig := &TopicConfig{
+		TopicName:      topicName,
+		WriteQueueNums: writeQueueNums,
+		ReadQueueNums:  readQueueNums,
+		SEPARATOR:      separator,
+		Perm:           perm,
+		TopicSysFlag:   topicSysFlag,
+	}
+	if filterType != nil && len(filterType) > 0 {
+		topicConfig.TopicFilterType = filterType[0]
+	} else {
+		topicConfig.TopicFilterType = topicFilterType
+	}
+
 	return topicConfig
 }
 
@@ -47,6 +73,15 @@ func (self *TopicConfig) ToString() string {
 	if self == nil {
 		return ""
 	}
-	format := "TopicConfig [topicName=%s, readQueueNums=%d, writeQueueNums=%d, perm=%d, topicFilterType=%d, topicSysFlag=%d, order=%t]"
-	return fmt.Sprintf(format, self.TopicName, self.ReadQueueNums, self.WriteQueueNums, self.Perm, int(self.TopicFilterType), self.TopicSysFlag, self.Order)
+
+	filterType := int(self.TopicFilterType)
+	format := "TopicConfig [topicName=%s, readQueueNums=%d, writeQueueNums=%d, perm=%s, topicFilterType=%d, topicSysFlag=%d, order=%t]"
+	return fmt.Sprintf(format, self.TopicName, self.ReadQueueNums, self.WriteQueueNums, self.ToPermString(), filterType, self.TopicSysFlag, self.Order)
+}
+
+func (self *TopicConfig) ToPermString() string {
+	if self == nil {
+		return ""
+	}
+	return fmt.Sprintf("%d(%s)", self.Perm, constant.Perm2String(self.Perm))
 }
