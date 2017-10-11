582657b8fea87104d61033583b8f67f4db552030
idistyl <idistyl@gmail.com>
2017-10-11 21:10:09 +0800

    update 优化namesrv日志

--- stgcommon/protocol/heartbeat/subscription_data.go
+++ stgcommon/protocol/heartbeat/subscription_data.go
@@ -5,7 +5,6 @@ import set "github.com/deckarep/golang-set"
 // SubscriptionData: 订阅信息结构体
 // Author: yintongqiang
 // Since:  2017/8/9
-
 type SubscriptionData struct {
 	SUB_ALL         string  `json:"-"`
 	ClassFilterMode bool    `json:"classFilterMode"`
