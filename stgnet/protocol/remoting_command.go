afdca03a6dfcbfbfb0b51e9f077e06585c8b6363
jerrylou <gunsluo@gmail.com>
2017-10-12 13:27:29 +0800

    modify error info
    Signed-off-by: jerrylou <gunsluo@gmail.com>

--- stgnet/protocol/remoting_command.go
+++ stgnet/protocol/remoting_command.go
@@ -214,7 +214,7 @@ func DecodeRemotingCommand(buf *bytes.Buffer) (*RemotingCommand, error) {
 
 	// step 1 读取报文长度
 	if buf.Len() < 4 {
-		return nil, errors.Errorf("DecodeRemotingCommand: buffer length[%d] incorrect，minimal is 4", buf.Len())
+		return nil, errors.Errorf("DecodeRemotingCommand: buffer-length[%d] incorrect，minimal is 4", buf.Len())
 	}
 
 	err := binary.Read(buf, binary.BigEndian, &length)
@@ -224,7 +224,7 @@ func DecodeRemotingCommand(buf *bytes.Buffer) (*RemotingCommand, error) {
 
 	// step 2 读取报文头长度
 	if buf.Len() < 4 {
-		return nil, errors.Errorf("DecodeRemotingCommand: attribute header-length[%d] form buffer incorrect，minimal is 4", buf.Len())
+		return nil, errors.Errorf("DecodeRemotingCommand: buffer-length[%d] < header-length[%d](length is 4).", buf.Len(), 4)
 	}
 
 	err = binary.Read(buf, binary.BigEndian, &headerLength)
@@ -234,7 +234,7 @@ func DecodeRemotingCommand(buf *bytes.Buffer) (*RemotingCommand, error) {
 
 	// step 3 读取报文头数据
 	if buf.Len() == 0 || buf.Len() < int(headerLength) {
-		return nil, errors.Errorf("DecodeRemotingCommand: buffer length[%d] < attribute header-data[%d] from buffer.", buf.Len(), headerLength)
+		return nil, errors.Errorf("DecodeRemotingCommand: buffer-length[%d] < attribute header-data[%d] from buffer.", buf.Len(), headerLength)
 	}
 
 	header := make([]byte, headerLength)
@@ -246,7 +246,7 @@ func DecodeRemotingCommand(buf *bytes.Buffer) (*RemotingCommand, error) {
 	// step 4 读取报文Body
 	bodyLength = length - 4 - headerLength
 	if buf.Len() < int(bodyLength) {
-		return nil, errors.Errorf("DecodeRemotingCommand: buffer length[%d] < attribute body[%d] from buffer.", buf.Len(), bodyLength)
+		return nil, errors.Errorf("DecodeRemotingCommand: buffer-length[%d] < attribute body[%d] from buffer.", buf.Len(), bodyLength)
 	}
 
 	if bodyLength > 0 {
