e8b730bd83a772770ab3ed3c0a90a4e9d57c8803
yintongjiang <261026193@qq.com>
2017-10-13 16:31:41 +0800

    update

--- example/stgclient/consumer/push/consumer_push.go
+++ example/stgclient/consumer/push/consumer_push.go
@@ -8,13 +8,22 @@ import (
 	"git.oschina.net/cloudzone/smartgo/stgcommon/message"
 	"git.oschina.net/cloudzone/smartgo/stgcommon/protocol/heartbeat"
 	"time"
+	"sync/atomic"
 )
 
 type MessageListenerImpl struct {
+	MsgCount int64
+	StartTime int64
 }
 
 func (listenerImpl *MessageListenerImpl) ConsumeMessage(msgs []*message.MessageExt, context *consumer.ConsumeConcurrentlyContext) listener.ConsumeConcurrentlyStatus {
 	for _, msg := range msgs {
+		atomic.AddInt64(&listenerImpl.MsgCount, 1)
+		if listenerImpl.MsgCount==500000{
+			fmt.Println(500000/(time.Now().Unix()-listenerImpl.StartTime),"______________________________________")
+
+		}
+		fmt.Println(listenerImpl.MsgCount)
 		fmt.Println(msg.ToString())
 	}
 	return listener.CONSUME_SUCCESS
@@ -31,12 +40,12 @@ func taskC() {
 }
 
 func main() {
-	defaultMQPushConsumer := process.NewDefaultMQPushConsumer("consume9991a211")
+	defaultMQPushConsumer := process.NewDefaultMQPushConsumer("consume9s991a2a11")
 	defaultMQPushConsumer.SetConsumeFromWhere(heartbeat.CONSUME_FROM_LAST_OFFSET)
 	defaultMQPushConsumer.SetMessageModel(heartbeat.CLUSTERING)
 	defaultMQPushConsumer.SetNamesrvAddr("10.112.68.189:9876")
-	defaultMQPushConsumer.Subscribe("cloudzone4", "tagA")
-	defaultMQPushConsumer.RegisterMessageListener(&MessageListenerImpl{})
+	defaultMQPushConsumer.Subscribe("cloudzone1", "tagA")
+	defaultMQPushConsumer.RegisterMessageListener(&MessageListenerImpl{StartTime:time.Now().Unix()})
 	defaultMQPushConsumer.Start()
 	go taskC()
 	select {}
