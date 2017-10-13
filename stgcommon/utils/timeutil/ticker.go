ee0264bc3a65010fd145f4759168592b10167039
jerrylou <gunsluo@gmail.com>
2017-10-13 19:48:17 +0800

    modify ticker stop with delete sys timer
    Signed-off-by: jerrylou <gunsluo@gmail.com>

--- stgcommon/utils/timeutil/ticker.go
+++ stgcommon/utils/timeutil/ticker.go
@@ -48,6 +48,7 @@ func (t *Ticker) start() {
 			t.fn()
 			if !t.isRun {
 				if t.over != nil {
+					t.tm.Stop()
 					close(t.over)
 				}
 				return
@@ -75,6 +76,5 @@ func (t *Ticker) Stop() bool {
 	if t.over != nil {
 		<-t.over
 	}
-	t.tm.Stop()
 	return true
 }
