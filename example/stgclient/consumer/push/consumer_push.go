3381779813b7b353e68676bbfdcc6a2c1bc21a6d
idistyl <idistyl@gmail.com>
2017-10-19 20:30:18 +0800

    update 优化broker以及client启动example示例

--- example/stgclient/consumer/push/consumer_push.go
+++ example/stgclient/consumer/push/consumer_push.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"git.oschina.net/cloudzone/smartgo/stgclient/consumer"
 	"git.oschina.net/cloudzone/smartgo/stgclient/consumer/listener"
@@ -45,18 +46,24 @@ func taskC() {
 }
 
 var (
-	namesrvAddr     = "10.112.68.89:9876"
-	topic           = "cloudzone1"
-	tag             = "tagA"
-	consumerGroupId = "consumerGroupId-200"
+	def_consumerGroupId = "consumerGroupId-200"
+	def_topic           = "cloudzone123"
+	def_namesrvAddr     = "10.112.68.189:9876"
+	def_tag             = "tagA"
 )
 
 func main() {
-	defaultMQPushConsumer := process.NewDefaultMQPushConsumer(consumerGroupId)
+	consumerGroupId := flag.String("p", def_consumerGroupId, "the consumer group id ")
+	topic := flag.String("topic", def_topic, "the topic for use")
+	tag := flag.String("tag", def_tag, "the tag for use")
+	namesrvAddr := flag.String("h", def_namesrvAddr, "the namesrv ip:port")
+	flag.Parse()
+
+	defaultMQPushConsumer := process.NewDefaultMQPushConsumer(*consumerGroupId)
 	defaultMQPushConsumer.SetConsumeFromWhere(heartbeat.CONSUME_FROM_LAST_OFFSET)
 	defaultMQPushConsumer.SetMessageModel(heartbeat.CLUSTERING)
-	defaultMQPushConsumer.SetNamesrvAddr(namesrvAddr)
-	defaultMQPushConsumer.Subscribe(topic, tag)
+	defaultMQPushConsumer.SetNamesrvAddr(*namesrvAddr)
+	defaultMQPushConsumer.Subscribe(*topic, *tag)
 	defaultMQPushConsumer.RegisterMessageListener(&MessageListenerImpl{StartTime: time.Now().Unix(), MapContent: sync.NewMap()})
 	defaultMQPushConsumer.Start()
 	go taskC()
