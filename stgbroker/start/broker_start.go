4501902fcfb718cc9b395596c1d32e99bcd25f78
idistyl <idistyl@gmail.com>
2017-10-24 20:01:54 +0800

    update 优化broker启动

--- stgbroker/start/broker_start.go
+++ stgbroker/start/broker_start.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"git.oschina.net/cloudzone/smartgo/stgbroker"
 	"git.oschina.net/cloudzone/smartgo/stgcommon/mqversion"
+	"github.com/toolkits/file"
 	"os"
 )
 
@@ -35,7 +36,12 @@ func main() {
 	// 没有使用-c指令，那么*c="configPath"表示默认值
 	cfgPath := ""
 	if *c != "configPath" {
-		cfgPath = *c
+		if !file.IsExist(*c) {
+			fmt.Println("use -c to valid broker config path. eg: -c /home/smartgo-bin/conf/broker-a.toml")
+			os.Exit(0)
+		} else {
+			cfgPath = *c
+		}
 	}
 
 	stopChannel := make(chan bool, 1) // the 'stopChannel' variable to handle controller.shutdownHook()
