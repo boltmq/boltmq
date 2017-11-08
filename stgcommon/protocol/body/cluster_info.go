75d591b044cc89ae0d1f69e2edd4f93e5c86ac6c
idistyl <idistyl@gmail.com>
2017-11-08 17:32:40 +0800

    feature: 完成创建topic接口

--- stgcommon/protocol/body/cluster_info.go
+++ stgcommon/protocol/body/cluster_info.go
@@ -10,19 +10,71 @@ import (
 // Author: tianyuliang, <tianyuliang@gome.com.cn>
 // Since: 2017/9/4
 type ClusterInfo struct {
-	BokerAddrTable   map[string]*route.BrokerData `json:"bokerAddrTable"`   // brokerName[BrokerData]
+	BokerAddrTable   map[string]*route.BrokerData `json:"brokerAddrTable"`  // brokerName[BrokerData]
 	ClusterAddrTable map[string]set.Set           `json:"clusterAddrTable"` // clusterName[set<brokerName>]
 	*protocol.RemotingSerializable
 }
 
+// ClusterPlusInfo 协议中传输对象，内容为集群信息
+//
+// 注意: set.Set类型在反序列化过程无法解析，因此额外设置ClusterPlusInfo类型来解析
+//
+// Author: tianyuliang, <tianyuliang@gome.com.cn>
+// Since: 2017/9/4
+type ClusterPlusInfo struct {
+	BokerAddrTable   map[string]*route.BrokerData `json:"brokerAddrTable"`  // brokerName[BrokerData]
+	ClusterAddrTable map[string][]string          `json:"clusterAddrTable"` // clusterName[set<brokerName>]
+	*protocol.RemotingSerializable
+}
+
+// NewClusterInfo 初始化
+// Author: tianyuliang, <tianyuliang@gome.com.cn>
+// Since: 2017/9/4
 func NewClusterInfo() *ClusterInfo {
 	clusterInfo := &ClusterInfo{
-		BokerAddrTable:   make(map[string]*route.BrokerData, 0),
-		ClusterAddrTable: make(map[string]set.Set, 0),
+		BokerAddrTable:   make(map[string]*route.BrokerData),
+		ClusterAddrTable: make(map[string]set.Set),
 	}
 	return clusterInfo
 }
 
+// NewClusterPlusInfo 初始化
+// Author: tianyuliang, <tianyuliang@gome.com.cn>
+// Since: 2017/9/4
+func NewClusterPlusInfo() *ClusterPlusInfo {
+	clusterPlusInfo := &ClusterPlusInfo{
+		BokerAddrTable:   make(map[string]*route.BrokerData),
+		ClusterAddrTable: make(map[string][]string),
+	}
+	return clusterPlusInfo
+}
+
+// ToClusterInfo 转化为 ClusterInfo 类型
+// Author: tianyuliang, <tianyuliang@gome.com.cn>
+// Since: 2017/11/8
+func (plus *ClusterPlusInfo) ToClusterInfo() *ClusterInfo {
+	if plus == nil {
+		return nil
+	}
+	clusterInfo := NewClusterInfo()
+	clusterInfo.BokerAddrTable = plus.BokerAddrTable
+	if plus.ClusterAddrTable != nil {
+		for clusterName, brokerNames := range plus.ClusterAddrTable {
+			brokerNameSet := set.NewSet()
+			if brokerNames != nil && len(brokerNames) > 0 {
+				for _, brokerName := range brokerNames {
+					brokerNameSet.Add(brokerName)
+				}
+			}
+			clusterInfo.ClusterAddrTable[clusterName] = brokerNameSet
+		}
+	}
+	return clusterInfo
+}
+
+// RetrieveAllClusterNames 处理所有brokerName名称
+// Author: tianyuliang, <tianyuliang@gome.com.cn>
+// Since: 2017/9/4
 func (self *ClusterInfo) RetrieveAllClusterNames() []string {
 	if self.ClusterAddrTable == nil || len(self.ClusterAddrTable) == 0 {
 		return []string{}
@@ -39,6 +91,10 @@ func (self *ClusterInfo) RetrieveAllClusterNames() []string {
 
 	return brokerNames
 }
+
+// RetrieveAllAddrByCluster 处理所有brokerAddr地址
+// Author: tianyuliang, <tianyuliang@gome.com.cn>
+// Since: 2017/9/4
 func (self *ClusterInfo) RetrieveAllAddrByCluster(clusterName string) []string {
 	if self.ClusterAddrTable == nil || len(self.ClusterAddrTable) == 0 {
 		return []string{}
