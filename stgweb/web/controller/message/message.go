02f83092d7402d80e384573b0511028e6564b24e
idistyl <idistyl@gmail.com>
2017-11-09 10:35:24 +0800

    feature: add all interface to blotmq web controller

--- stgweb/web/controller/message/message.go
+++ stgweb/web/controller/message/message.go
@@ -1,1 +1,27 @@
 package message
+
+import (
+	"git.oschina.net/cloudzone/cloudcommon-go/web/resp"
+	"github.com/kataras/iris/context"
+)
+
+// MessageBody 查询消息内容
+// Author: tianyuliang, <tianyuliang@gome.com.cn>
+// Since: 2017/11/9
+func MessageBody(ctx context.Context) {
+	ctx.JSON(resp.NewSuccessResponse(""))
+}
+
+// MessageTrack 查询消息消费轨迹
+// Author: tianyuliang, <tianyuliang@gome.com.cn>
+// Since: 2017/11/9
+func MessageTrack(ctx context.Context) {
+	ctx.JSON(resp.NewSuccessResponse(""))
+}
+
+// MessageQuery 查询消息消费结果
+// Author: tianyuliang, <tianyuliang@gome.com.cn>
+// Since: 2017/11/9
+func MessageQuery(ctx context.Context) {
+	ctx.JSON(resp.NewSuccessResponse(""))
+}
--- stgweb/web/route/route.go
+++ stgweb/web/route/route.go
@@ -2,6 +2,12 @@ package route
 
 import (
 	"git.oschina.net/cloudzone/cloudcommon-go/web"
+	"git.oschina.net/cloudzone/smartgo/stgregistry/other"
+	"git.oschina.net/cloudzone/smartgo/stgweb/web/controller/broker"
+	"git.oschina.net/cloudzone/smartgo/stgweb/web/controller/cluster"
+	"git.oschina.net/cloudzone/smartgo/stgweb/web/controller/general"
+	"git.oschina.net/cloudzone/smartgo/stgweb/web/controller/group"
+	"git.oschina.net/cloudzone/smartgo/stgweb/web/controller/message"
 	"git.oschina.net/cloudzone/smartgo/stgweb/web/controller/topic"
 	"github.com/kataras/iris/context"
 )
@@ -13,35 +19,52 @@ func Route(ctx *web.Context) error {
 		api.Options("/{root:path}", func(ctx context.Context) {}) // fix options not match bug
 	}
 
-	// 概览
+	// 整体概览
 	{
-		//api.Get("/general", general.General)
+		api.Get("/general", general.GeneralStats)
 	}
 
 	// cluster集群管理
 	{
-
+		api.Get("/cluster/general", cluster.ClusterGeneral)
+		api.Get("/cluster/list", cluster.ClusterList)
 	}
 
 	// topic管理
 	{
 		api.Post("/topic", topic.CreateTopic)
+		api.Put("/topic", topic.UpdateTopic)
+		api.Delete("/topic", topic.DeleteTopic)
 		api.Get("/topic/list", topic.TopicList)
+		api.Get("/topic/stats", topic.TopicStats)
+		api.Get("/topic/route", topic.TopicRoute)
 	}
 
 	// 消费进度
 	{
-
+		api.Get("/group/progress", group.ConnectionDetail)
+		api.Get("/group/list", group.ConsumeProgress)
 	}
 
-	// 消息查询
+	// 消费进程
 	{
+		api.Get("/connection/online", group.ConnectionOnline)
+		api.Get("/connection/detail", group.ConnectionDetail)
+	}
 
+	// 消息查询、消费轨迹
+	{
+		api.Get("/msg/body", message.MessageBody)
+		api.Get("/msg/track", message.MessageQuery)
+		api.Get("/msg/query", message.MessageQuery)
 	}
 
 	// 运维
 	{
-
+		api.Delete("/consumer/subGroup", broker.DeleteSubGroup)
+		api.Put("/consumer/subGroup", broker.UpdateSubGroup)
+		api.Post("/broker/syncTopic", broker.SyncTopicToBroker)
+		api.Post("/broker/wipePerm", broker.WipeWritePermBroker)
 	}
 
 	return nil
