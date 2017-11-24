49b376fa22d63eb820301823c26be43c3b17687d
luoji <gunsluo@gmail.com>
2017-11-24 11:49:55 +0800

    sync async sample
    Signed-off-by: luoji <gunsluo@gmail.com>

--- sample/net/core/multi/client/client.go
+++ sample/net/core/multi/client/client.go
@@ -43,11 +43,13 @@ func (listener *clientEventListener) OnContextConnect(ctx core.Context) {
 func (listener *clientEventListener) OnContextClosed(ctx core.Context) {
 	//listener.closed++
 	atomic.AddInt64(&listener.closed, 1)
+	ctx.Close()
 }
 
 func (listener *clientEventListener) OnContextError(ctx core.Context, err error) {
-	listener.errors++
-	//atomic.AddInt64(&listener.errors, 1)
+	//listener.errors++
+	atomic.AddInt64(&listener.errors, 1)
+	ctx.Close()
 }
 
 func (listener *clientEventListener) Contexts() []core.Context {
@@ -62,10 +64,21 @@ func (listener *clientEventListener) Contexts() []core.Context {
 	return contexts
 }
 
+type responseFuture struct {
+	sendTime time.Time
+	done     chan struct{}
+}
+
+type responseManager struct {
+	responseTableMu sync.RWMutex
+	responseTable   map[core.SocketAddr]*responseFuture
+}
+
 func main() {
 	debug.SetMaxThreads(100000)
 	sraddr := flag.String("r", "10.128.31.108:8000", "remote addr")
 	sladdr := flag.String("l", "10.128.31.108:0", "local addr")
+	sync := flag.Bool("s", false, "sync or async")
 	mcn := flag.Int64("c", 50000, "max connect num")
 	flag.Parse()
 
@@ -73,12 +86,14 @@ func main() {
 		maxConnNum   = *mcn
 		receMsgTotal int32
 		cd           time.Duration
-		hbs          int
-		hbf          int
+		hbs          int64
+		hbf          int64
 		maxTime      time.Duration
 		minTime      time.Duration
 		averageTime  time.Duration
+		spendAllTime time.Duration
 		pingMsg      = []byte("Ping")
+		rm           = &responseManager{responseTable: make(map[core.SocketAddr]*responseFuture, *mcn)}
 	)
 
 	listener := &clientEventListener{}
@@ -86,6 +101,31 @@ func main() {
 	b := core.NewBootstrap().SetReadBufferSize(512).SetEventListener(listener)
 	b.RegisterHandler(func(buffer []byte, ctx core.Context) {
 		atomic.AddInt32(&receMsgTotal, 1)
+
+		socketAddr := ctx.LocalAddrToSocketAddr()
+		rm.responseTableMu.RLock()
+		respFuture, ok := rm.responseTable[*socketAddr]
+		if !ok {
+			rm.responseTableMu.RUnlock()
+			return
+		}
+		rm.responseTableMu.RUnlock()
+
+		if *sync {
+			respFuture.done <- struct{}{}
+		}
+
+		spendTime := time.Now().Sub(respFuture.sendTime)
+		if spendTime > maxTime {
+			maxTime = spendTime
+		}
+		if spendTime < minTime || minTime == time.Duration(0) {
+			minTime = spendTime
+		}
+		spendAllTime += spendTime
+		if hbs != 0 {
+			averageTime = time.Duration(int64(spendAllTime) / hbs)
+		}
 	})
 
 	// 创建连接
@@ -109,7 +149,7 @@ func main() {
 			<-timer.C
 			for i := 0; i < len(ctxs); i++ {
 				ctx := ctxs[i]
-				_, err := ctx.Write(pingMsg)
+				err := sendMsg(rm, *sync, ctx, pingMsg)
 				if err != nil {
 					hbf++
 					//log.Printf("heartbeat faild: %s\n", err)
@@ -139,4 +179,29 @@ func main() {
 	}()
 
 	select {}
+}
+
+func sendMsg(rm *responseManager, sync bool, ctx core.Context, msg []byte) error {
+	socketAddr := ctx.LocalAddrToSocketAddr()
+	rm.responseTableMu.Lock()
+	respFuture, ok := rm.responseTable[*socketAddr]
+	if !ok {
+		respFuture = &responseFuture{
+			done: make(chan struct{}),
+		}
+		rm.responseTable[*socketAddr] = respFuture
+	}
+	rm.responseTableMu.Unlock()
+
+	respFuture.sendTime = time.Now()
+	_, err := ctx.Write(msg)
+	if err != nil {
+		return err
+	}
+
+	if sync {
+		<-respFuture.done
+	}
+
+	return nil
 }
