7d4326276f20d4877cc5dfba83fe49bcd1beec42
luoji <gunsluo@gmail.com>
2017-12-25 23:15:56 +0800

    message store interface
    Signed-off-by: luoji <gunsluo@gmail.com>

--- store/message_store.go
+++ store/message_store.go
@@ -14,6 +14,7 @@
 package store
 
 import (
+	"github.com/boltmq/boltmq/store/core"
 	"github.com/boltmq/common/message"
 	"github.com/boltmq/common/protocol/heartbeat"
 )
@@ -22,19 +23,19 @@ import (
 // Author zhoufei
 // Since 2017/9/6
 type MessageStore interface {
-	Load() bool                                              //
-	Start() error                                            //
-	Shutdown()                                               // 关闭存储服务
-	Destroy()                                                //
-	PutMessage(msg *MessageExtBrokerInner) *PutMessageResult //
-	GetMessage(group string, topic string, queueId int32, offset int64, maxMsgNums int32, subscriptionData *heartbeat.SubscriptionData) *GetMessageResult
+	Load() bool                                                   //
+	Start() error                                                 //
+	Shutdown()                                                    // 关闭存储服务
+	Destroy()                                                     //
+	PutMessage(msg *MessageExtBrokerInner) *core.PutMessageResult //
+	GetMessage(group string, topic string, queueId int32, offset int64, maxMsgNums int32, subscriptionData *heartbeat.SubscriptionData) *core.GetMessageResult
 	GetMaxOffsetInQueue(topic string, queueId int32) int64                                                    // 获取指定队列最大Offset 如果队列不存在，返回-1
 	GetMinOffsetInQueue(topic string, queueId int32) int64                                                    // 获取指定队列最小Offset 如果队列不存在，返回-1
 	GetCommitLogOffsetInQueue(topic string, queueId int32, cqOffset int64) int64                              //
 	GetOffsetInQueueByTime(topic string, queueId int32, timestamp int64) int64                                // 根据消息时间获取某个队列中对应的offset
 	LookMessageByOffset(commitLogOffset int64) *message.MessageExt                                            // 通过物理队列Offset，查询消息。 如果发生错误，则返回null
-	SelectOneMessageByOffset(commitLogOffset int64) *SelectMapedBufferResult                                  // 通过物理队列Offset，查询消息。 如果发生错误，则返回null
-	SelectOneMessageByOffsetAndSize(commitLogOffset int64, msgSize int32) *SelectMapedBufferResult            // 通过物理队列Offset、size，查询消息。 如果发生错误，则返回null
+	SelectOneMessageByOffset(commitLogOffset int64) *core.SelectMapedBufferResult                             // 通过物理队列Offset，查询消息。 如果发生错误，则返回null
+	SelectOneMessageByOffsetAndSize(commitLogOffset int64, msgSize int32) *core.SelectMapedBufferResult       // 通过物理队列Offset、size，查询消息。 如果发生错误，则返回null
 	GetRunningDataInfo() string                                                                               //
 	GetRuntimeInfo() map[string]string                                                                        // 取运行时统计数据
 	GetMaxPhyOffset() int64                                                                                   //获取物理队列最大offset
@@ -42,10 +43,10 @@ type MessageStore interface {
 	GetEarliestMessageTime(topic string, queueId int32) int64                                                 // 获取队列中最早的消息时间
 	GetMessageStoreTimeStamp(topic string, queueId int32, offset int64) int64                                 //
 	GetMessageTotalInQueue(topic string, queueId int32) int64                                                 //
-	GetCommitLogData(offset int64) *SelectMapedBufferResult                                                   // 数据复制使用：获取CommitLog数据
+	GetCommitLogData(offset int64) *core.SelectMapedBufferResult                                              // 数据复制使用：获取CommitLog数据
 	AppendToCommitLog(startOffset int64, data []byte) bool                                                    // 数据复制使用：向CommitLog追加数据，并分发至各个Consume Queue
 	ExcuteDeleteFilesManualy()                                                                                //
-	QueryMessage(topic string, key string, maxNum int32, begin int64, end int64) *QueryMessageResult          //
+	QueryMessage(topic string, key string, maxNum int32, begin int64, end int64) *core.QueryMessageResult     //
 	UpdateHaMasterAddress(newAddr string)                                                                     //
 	SlaveFallBehindMuch() int64                                                                               // Slave落后Master多少，单位字节
 	Now() int64                                                                                               //
