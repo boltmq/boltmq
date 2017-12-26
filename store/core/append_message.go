9b196cba57a11ca7ba3b55be617b69c7f5df96c3
luoji <gunsluo@gmail.com>
2017-12-26 10:30:32 +0800

    rename file
    Signed-off-by: luoji <gunsluo@gmail.com>

--- store/core/append_message.go
+++ store/core/append_message.go
@@ -1,4 +1,4 @@
-// Copyright 2017 tantexian
+// Copyright 2017 gaoyanlei
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,10 +13,49 @@
 // limitations under the License.
 package core
 
+const (
+	APPENDMESSAGE_PUT_OK AppendMessageStatus = iota
+	END_OF_FILE
+	MESSAGE_SIZE_EXCEEDED
+	APPENDMESSAGE_UNKNOWN_ERROR
+)
+
 // AppendMessageCallback 写消息回调接口
 // Author: tantexian, <tantexian@qq.com>
 // Since: 2017/8/6
 type AppendMessageCallback interface {
 	// write MapedByteBuffer,and return How many bytes to write
 	doAppend(fileFromOffset int64, mappedByteBuffer *MappedByteBuffer, maxBlank int32, msg interface{}) *AppendMessageResult
+}
+
+// AppendMessageResult 写入commitlong返回结果集
+// Author gaoyanlei
+// Since 2017/8/16
+type AppendMessageResult struct {
+	Status         AppendMessageStatus
+	WroteOffset    int64
+	WroteBytes     int64
+	MsgId          string
+	StoreTimestamp int64
+	LogicsOffset   int64
+}
+
+// AppendMessageStatus 写入commitlog 返回code
+// Author gaoyanlei
+// Since 2017/8/16
+type AppendMessageStatus int
+
+func (status AppendMessageStatus) String() string {
+	switch status {
+	case APPENDMESSAGE_PUT_OK:
+		return "PUT_OK"
+	case END_OF_FILE:
+		return "END_OF_FILE"
+	case MESSAGE_SIZE_EXCEEDED:
+		return "MESSAGE_SIZE_EXCEEDED"
+	case APPENDMESSAGE_UNKNOWN_ERROR:
+		return "UNKNOWN_ERROR"
+	default:
+		return "Unknow"
+	}
 }
